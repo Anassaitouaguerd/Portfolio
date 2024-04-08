ffcc15256559b6683ebc7e9cd85dbefcbe72cef3
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-08 23:30:40 +0000

    consumption Formations api and show the data

--- React/src/components/SectionsComponents/SectionFormation.jsx
+++ React/src/components/SectionsComponents/SectionFormation.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 export default function SectionFormation() {
+  const [formations, setFormations] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch("http://127.0.0.1:8000/api/formations")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setFormations(data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
   return (
     <section data-scroll-index="1" className="services-3-area">
       <div className="container custom-container">
@@ -24,74 +47,25 @@ export default function SectionFormation() {
         <div className="row justify-content-end">
           <div className="col-lg-7">
             <div className="row">
-              <div className="col-md-6">
-                <div className="services-3-item mt-40">
-                  <div className="icon">
-                    <img
-                      // src={
-                      //   // require("./assets/images/icon/services-1.png").default
-                      // }
-                      alt="icon"
-                    />
+              {formations.map((formation, index) => (
+                <div className="col-md-6" key={index}>
+                  <div className="services-3-item mt-40">
+                    <div className="icon">
+                      <img
+                        // src={
+                        //   // require("./assets/images/icon/services-1.png").default
+                        // }
+                        alt="icon"
+                      />
+                    </div>
+                    <h5 className="title">{formation.name}</h5>
+                    <p>Comapny : {formation.company}</p>
+                    <p>Lieu : {formation.lieu}</p>
+                    <p>Date Start : {formation.Date_start}</p>
+                    <p>Date end : {formation.Date_end}</p>
                   </div>
-                  <h5 className="title">Curated content</h5>
-                  <p>
-                    Home insurance is a type of insurance policy that provides
-                    cover for your home.
-                  </p>
                 </div>
-              </div>
-              <div className="col-md-6">
-                <div className="services-3-item mt-40">
-                  <div className="icon">
-                    <img
-                      // src={
-                      //   // require("./assets/images/icon/services-2.png").default
-                      // }
-                      alt="icon"
-                    />
-                  </div>
-                  <h5 className="title">Curated content</h5>
-                  <p>
-                    Home insurance is a type of insurance policy that provides
-                    cover for your home.
-                  </p>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="services-3-item mt-40">
-                  <div className="icon">
-                    <img
-                      // src={
-                      //   // require("./assets/images/icon/services-3.png").default
-                      // }
-                      alt="icon"
-                    />
-                  </div>
-                  <h5 className="title">Curated content</h5>
-                  <p>
-                    Home insurance is a type of insurance policy that provides
-                    cover for your home.
-                  </p>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="services-3-item mt-40">
-                  <div className="icon">
-                    <img
-                      // src={
-                      //   // require("./assets/images/icon/services-4.png").default
-                      // }
-                      alt="icon"
-                    />
-                  </div>
-                  <h5 className="title">Curated content</h5>
-                  <p>
-                    Home insurance is a type of insurance policy that provides
-                    cover for your home.
-                  </p>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
