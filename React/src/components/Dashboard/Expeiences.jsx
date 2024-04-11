901a48d34685935f1bb131e21d0ec0f3231f2c0e
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-11 22:05:37 +0000

    the component experience

---
+++ React/src/components/Dashboard/Expeiences.jsx
@@ -0,0 +1,139 @@
+
+export default function Experiences()
+{
+  return (
+    <div className="col-12 mt-4">
+    <div className="card mb-4">
+      <div className="card-header pb-0 p-3">
+        <h6 className="mb-1">Projects</h6>
+        <p className="text-sm">Architects design houses</p>
+      </div>
+      <div className="card-body p-3">
+        <div className="row">
+          <div className="col-xl-3 col-md-6 mb-xl-0 mb-4">
+            <div className="card card-blog card-plain">
+              <div className="position-relative">
+                <a className="d-block shadow-xl border-radius-xl">
+                  <img src="../assets/img/home-decor-1.jpg" alt="img-blur-shadow" className="img-fluid shadow border-radius-xl" />
+                </a>
+              </div>
+              <div className="card-body px-1 pb-0">
+                <p className="text-gradient text-dark mb-2 text-sm">Project #2</p>
+                <a href="javascript:;">
+                  <h5>
+                    Modern
+                  </h5>
+                </a>
+                <p className="mb-4 text-sm">
+                  As Uber works through a huge amount of internal management turmoil.
+                </p>
+                <div className="d-flex align-items-center justify-content-between">
+                  <button type="button" className="btn btn-outline-primary btn-sm mb-0">View Project</button>
+                  <div className="avatar-group mt-2">
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
+                      <img alt="Image placeholder" src="../assets/img/team-1.jpg" />
+                    </a>
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
+                      <img alt="Image placeholder" src="../assets/img/team-2.jpg" />
+                    </a>
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
+                      <img alt="Image placeholder" src="../assets/img/team-3.jpg" />
+                    </a>
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
+                      <img alt="Image placeholder" src="../assets/img/team-4.jpg" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-md-6 mb-xl-0 mb-4">
+            <div className="card card-blog card-plain">
+              <div className="position-relative">
+                <a className="d-block shadow-xl border-radius-xl">
+                  <img src="../assets/img/home-decor-2.jpg" alt="img-blur-shadow" className="img-fluid shadow border-radius-lg" />
+                </a>
+              </div>
+              <div className="card-body px-1 pb-0">
+                <p className="text-gradient text-dark mb-2 text-sm">Project #1</p>
+                <a href="javascript:;">
+                  <h5>
+                    Scandinavian
+                  </h5>
+                </a>
+                <p className="mb-4 text-sm">
+                  Music is something that every person has his or her own specific opinion about.
+                </p>
+                <div className="d-flex align-items-center justify-content-between">
+                  <button type="button" className="btn btn-outline-primary btn-sm mb-0">View Project</button>
+                  <div className="avatar-group mt-2">
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
+                      <img alt="Image placeholder" src="../assets/img/team-3.jpg" />
+                    </a>
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
+                      <img alt="Image placeholder" src="../assets/img/team-4.jpg" />
+                    </a>
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
+                      <img alt="Image placeholder" src="../assets/img/team-1.jpg" />
+                    </a>
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
+                      <img alt="Image placeholder" src="../assets/img/team-2.jpg" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-md-6 mb-xl-0 mb-4">
+            <div className="card card-blog card-plain">
+              <div className="position-relative">
+                <a className="d-block shadow-xl border-radius-xl">
+                  <img src="../assets/img/home-decor-3.jpg" alt="img-blur-shadow" className="img-fluid shadow border-radius-xl" />
+                </a>
+              </div>
+              <div className="card-body px-1 pb-0">
+                <p className="text-gradient text-dark mb-2 text-sm">Project #3</p>
+                <a href="javascript:;">
+                  <h5>
+                    Minimalist
+                  </h5>
+                </a>
+                <p className="mb-4 text-sm">
+                  Different people have different taste, and various types of music.
+                </p>
+                <div className="d-flex align-items-center justify-content-between">
+                  <button type="button" className="btn btn-outline-primary btn-sm mb-0">View Project</button>
+                  <div className="avatar-group mt-2">
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
+                      <img alt="Image placeholder" src="../assets/img/team-4.jpg" />
+                    </a>
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
+                      <img alt="Image placeholder" src="../assets/img/team-3.jpg" />
+                    </a>
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
+                      <img alt="Image placeholder" src="../assets/img/team-2.jpg" />
+                    </a>
+                    <a href="javascript:;" className="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
+                      <img alt="Image placeholder" src="../assets/img/team-1.jpg" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-md-6 mb-xl-0 mb-4">
+            <div className="card h-100 card-plain border">
+              <div className="card-body d-flex flex-column justify-content-center text-center">
+                <a href="javascript:;">
+                  <i className="fa fa-plus text-secondary mb-3"></i>
+                  <h5 className=" text-secondary"> New project </h5>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  ); 
+}
