f01544ae99f8c482b40efcded415e630eba1ca7b
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-11 22:10:09 +0000

    folder to the all components for the home side

---
+++ React/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
